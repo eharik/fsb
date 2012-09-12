30d351c9fdaf245b58b2547cd020b32126d3f056
Elliot P. Harik <elliot.harik@gmail.com>
2012-09-11 22:29:37 -0500

    Switch to NFL

--- app/models/game.rb
+++ app/models/game.rb
@@ -14,7 +14,7 @@ class Game < ActiveRecord::Base
   
   def self.update_scores
     puts "**********updating_game_scores --> #{Time.now} **************"
-    url = "http://espn.go.com/mlb/scoreboard"
+    url = "http://espn.go.com/nfl/scoreboard"
     doc = Nokogiri::HTML(open(url))
     
     home_teams = get_home_teams2(doc)
@@ -28,7 +28,7 @@ class Game < ActiveRecord::Base
   
   def self.update_games
     puts "**********updating_available_games**************"
-    url = "http://bovada.net/sports/odds/mlb"
+    url = "http://bovada.net/sports/odds/nfl"
     doc = Nokogiri::HTML(open(url))
   
     @home_teams = get_home_teams_bovada(doc)
@@ -117,7 +117,7 @@ class Game < ActiveRecord::Base
       doc.css(".home .runline div").each do |line|
         whole_part = line.text[/\-*[0-9]+/].to_f
         fraction_part = line.text[/\u00BD/] ? 0.5 : 0.0
-        if whole_part < 0
+        if whole_part < 0 
           temp = whole_part - fraction_part
         else
           temp = whole_part + fraction_part
@@ -236,23 +236,23 @@ class Game < ActiveRecord::Base
     
     def self.get_home_teams2(doc)
       teams = []
-      doc.css(".home .team-name a").each do |t|
-        teams << full_team_name_MLB(t.children.text)
+      doc.css(".home a").each do |t|
+        teams << full_team_name_NFL(t.children.text)
       end
       return teams
     end
     
     def self.get_away_teams2(doc)
       teams = []
-      doc.css(".away .team-name a").each do |t|
-        teams << full_team_name_MLB(t.children.text)
+      doc.css(".visitor a").each do |t|
+        teams << full_team_name_NFL(t.children.text)
       end
       return teams
     end
     
     def self.get_scores_home(doc)
       scores = []
-      doc.css(".home .finalScore").each do |s|
+      doc.css(".home .final").each do |s|
         scores << s.text.to_f
       end
       return scores
@@ -260,7 +260,7 @@ class Game < ActiveRecord::Base
     
     def self.get_scores_away(doc)
       scores = []
-      doc.css(".away .finalScore").each do |s|
+      doc.css(".visitor .final").each do |s|
         scores << s.children.text.to_f
       end
       return scores
@@ -307,135 +307,178 @@ class Game < ActiveRecord::Base
       return status
     end
     
-    def self.full_team_name_MLB ( team )
+    def self.full_team_name_NFL ( team )
       case team
-      when "Baltimore"        then return "Baltimore Orioles"
-      when "Orioles"          then return "Baltimore Orioles"
-      when 'Red Sox'          then return 'Boston Red Sox'
-      when 'Boston'           then return 'Boston Red Sox'
-      when 'Yankees'          then return 'New York Yankees'
-      #
-      when 'Rays'             then return 'Tampa Bay Rays'
-      when 'Tampa Bay'        then return 'Tampa Bay Rays'
-      when 'Blue Jays'        then return 'Toronto Blue Jays'
-      when 'Toronto'          then return 'Toronto Blue Jays'
-      when 'Braves'           then return 'Atlanta Braves'
-      when 'Atlanta'          then return 'Atlanta Braves'
-      when 'Marlins'          then return 'Florida Marlins'
-      when 'Miami'            then return 'Florida Marlins'
-      when 'Mets'             then return 'New York Mets'
-      #
-      when 'Phillies'         then return 'Philadelphia Phillies'
-      when 'Philadelphia'     then return 'Philadelphia Phillies'
-      when 'Nationals'        then return 'Washington Nationals'
-      when 'Washington'       then return 'Washington Nationals'
-      when 'White Sox'        then return 'Chicago White Sox'
-      #
-      when 'Indians'          then return 'Cleveland Indians'
-      when 'Cleveland'        then return 'Cleveland Indians'
-      when 'Tigers'           then return 'Detroit Tigers'
-      when 'Detroit'          then return 'Detroit Tigers'
-      when 'Royals'           then return 'Kansas City Royals'
-      when 'Kansas City'      then return 'Kansas City Royals'
-      when 'Twins'            then return 'Minnesota Twins'
-      when 'Minnesota'        then return 'Minnesota Twins'
-      when 'Cubs'             then return 'Chicago Cubs'
-      #
-      when 'Reds'             then return 'Cincinnati Reds'
-      when 'Cincinnati'       then return 'Cinccinati Reds'
-      when 'Astros'           then return 'Houston Astros'
-      when 'Houston'          then return 'Houston Astros'
-      when 'Brewers'          then return 'Milwaukee Brewers'
-      when 'Milwaukee'        then return 'Milwaukee Brewers'
-      when 'Pirates'          then return 'Pittsburgh Pirates'
-      when 'Pittsburgh'       then return 'Pittsburgh Pirates'
-      when 'Cardinals'        then return 'St. Louis Cardinals'
-      when 'St. Louis'        then return 'St. Louis Cardinals'
-      when 'Angels'           then return 'Los Angeles Angels'
-      #
-      when 'Athletics'        then return 'Oakland Athletics'
-      when 'Oakland'          then return 'Oakland Athletics'
-      when 'Mariners'         then return 'Seattle Mariners'
-      when 'Seattle'          then return 'Seattle Mariners'
-      when 'Rangers'          then return 'Texas Rangers'
-      when 'Texas'            then return 'Texas Rangers'
-      when 'Diamondbacks'     then return 'Arizona Diamondbacks'
-      when 'Arizona'          then return 'Arizona Diamondbacks'
-      when 'Rockies'          then return 'Colorado Rockies'
-      when 'Colorado'         then return 'Colorado Rockies'
-      when 'Dodgers'          then return 'Los Angeles Dodgers'
-      #
-      when 'Padres'           then return 'San Diego Padres'
-      when 'San Diego'        then return 'San Diego Padres'
-      when 'Giants'           then return 'San Francisco Giants'
-      when 'San Francisco'    then return 'San Francisco Giants'
+      # AFC East
+      when "Bills"        then return "Buffalo Bills"
+      when "Dolphins"     then return "Miami Dolphins"
+      when "Patriots"     then return "New England Patriots"
+      when "Jets"         then return "New York Jets"
+      # AFC West
+      when "Broncos"      then return "Denver Broncos"
+      when "Chiefs"       then return "Kansas City Chiefs"
+      when "Raiders"      then return "Oakland Raiders"
+      when "Chargers"     then return "San Diego Chargers"
+      # AFC North
+      when "Ravens"       then return "Baltimore Ravens"
+      when "Bengals"      then return "Cincinnati Bengals"
+      when "Browns"       then return "Cleveland Browns"
+      when "Steelers"     then return "Pittsburgh Steelers"
+      # AFC South
+      when "Texans"       then return "Houston Texans"
+      when "Colts"        then return "Indianapolis Colts"
+      when "Jaguars"      then return "Jacksonville Jaguars"
+      when "Titans"       then return "Tennessee Titans"
+      # NFC East
+      when "Cowboys"      then return "Dallas Cowboys"
+      when "Giants"       then return "New York Giants"
+      when "Eagles"       then return "Philadelphia Eagles"
+      when "Redskins"     then return "Washington Redskins"
+      # NFC West
+      when "Cardinals"    then return "Arizona Cardinals"
+      when "49ers"        then return "San Francisco 49ers"
+      when "Seahawks"     then return "Seattle Seahawks"
+      when "Rams"         then return "St. Louis Rams"
+      # NFC North
+      when "Bears"        then return "Chicago Bears"
+      when "Lions"        then return "Detroit Lions"
+      when "Packers"      then return "Green Bay Packers"
+      when "Vikings"      then return "Minnesota Vikings"
+      # NFC South
+      when "Falcons"      then return "Atlanta Falcons"
+      when "Panthers"     then return "Carolina Panthers"
+      when "Saints"       then return "New Orleans Saints"
+      when "Buccaneers"   then return "Tampa Bay Buccaneers"
       end
     end
     
-    def self.full_team_name_NBA( team )
-      case team
-        when "Atlanta"       then return "Atlanta Hawks"
-        when "Hawks"         then return "Atlanta Hawks"
-        when "Boston"        then return "Boston Celtics"
-        when "Celtics"       then return "Boston Celtics"
-        when "Charlotte"     then return "Charlotte Bobcats"
-        when "Bobcats"       then return "Charlotte Bobcats"
-        when "Chicago"       then return "Chicago Bulls"
-        when "Bulls"         then return "Chicago Bulls"
-        when "Cleveland"     then return "Cleveland Cavaliers"
-        when "Cavaliers"     then return "Cleveland Cavaliers"
-        when "Dallas"        then return "Dallas Mavericks"
-        when "Mavericks"     then return "Dallas Mavericks"
-        when "Denver"        then return "Denver Nuggets"
-        when "Nuggets"       then return "Denver Nuggets" 
-        when "Detroit"       then return "Detroit Pistons"
-        when "Pistons"       then return "Detroit Pistons"
-        when "Golden State"  then return "Golden State Warriors"
-        when "Warriors"      then return "Golden State Warriors"
-        when "Houston"       then return "Houston Rockets"
-        when "Rockets"       then return "Houston Rockets"
-        when "Indiana"       then return "Indiana Pacers"
-        when "Pacers"        then return "Indiana Pacers"
-        when "LA Clippers"   then return "Los Angeles Clippers"
-        when "Clippers"      then return "Los Angeles Clippers"
-        when "LA Lakers"     then return "Los Angeles Lakers"
-        when "Lakers"        then return "Los Angeles Lakers"
-        when "Memphis"       then return "Memphis Grizzlies"
-        when "Grizzlies"     then return "Memphis Grizzlies"
-        when "Miami"         then return "Miami Heat"
-        when "Heat"          then return "Miami Heat"
-        when "Milwaukee"     then return "Milwaukee Bucks"
-        when "Bucks"         then return "Milwaukee Bucks"
-        when "Minnesota"     then return "Minnesota Timberwolves"
-        when "Timberwolves"  then return "Minnesota Timberwolves"
-        when "New Jersey"    then return "New Jersey Nets"
-        when "Nets"          then return "New Jersey Nets"
-        when "New Orleans"   then return "New Orleans Hornets"
-        when "Hornets"       then return "New Orleans Hornets"
-        when "New York"      then return "New York Knicks"
-        when "NY Knicks"     then return "New York Knicks"
-        when "Knicks"        then return "New York Knicks"
-        when "Oklahoma City" then return "Oklahoma City Thunder"
-        when "Thunder"       then return "Oklahoma City Thunder"
-        when "Orlando"       then return "Orlando Magic"
-        when "Magic"         then return "Orlando Magic"
-        when "Philadelphia"  then return "Philadelphia 76ers"
-        when "76ers"         then return "Philadelphia 76ers"
-        when "Phoenix"       then return "Phoenix Suns"
-        when "Suns"          then return "Phoenix Suns"
-        when "Portland"      then return "Portland Trail Blazers"
-        when "Trail Blazers" then return "Portland Trail Blazers"
-        when "Sacramento"    then return "Sacramento Kings"
-        when "Kings"         then return "Sacramento Kings"
-        when "San Antonio"   then return "San Antonio Spurs"
-        when "Spurs"         then return "San Antonio Spurs"
-        when "Toronto"       then return "Toronto Raptors"
-        when "Raptors"       then return "Toronto Raptors"
-        when "Utah"          then return "Utah Jazz"
-        when "Jazz"          then return "Utah Jazz"
-        when "Washington"    then return "Washington Wizards"
-        when "Wizards"       then return "Washington Wizards"
-      end
-    end
+ #   def self.full_team_name_MLB ( team )
+ #     case team
+ #     when "Baltimore"        then return "Baltimore Orioles"
+ #     when "Orioles"          then return "Baltimore Orioles"
+ #     when 'Red Sox'          then return 'Boston Red Sox'
+ #     when 'Boston'           then return 'Boston Red Sox'
+ #     when 'Yankees'          then return 'New York Yankees'
+      #
+ #     when 'Rays'             then return 'Tampa Bay Rays'
+ #     when 'Tampa Bay'        then return 'Tampa Bay Rays'
+ #     when 'Blue Jays'        then return 'Toronto Blue Jays'
+ #     when 'Toronto'          then return 'Toronto Blue Jays'
+ #     when 'Braves'           then return 'Atlanta Braves'
+ #     when 'Atlanta'          then return 'Atlanta Braves'
+ #     when 'Marlins'          then return 'Florida Marlins'
+ #     when 'Miami'            then return 'Florida Marlins'
+ #     when 'Mets'             then return 'New York Mets'
+      #
+ #     when 'Phillies'         then return 'Philadelphia Phillies'
+ #     when 'Philadelphia'     then return 'Philadelphia Phillies'
+ #     when 'Nationals'        then return 'Washington Nationals'
+ #     when 'Washington'       then return 'Washington Nationals'
+ #     when 'White Sox'        then return 'Chicago White Sox'
+      #
+ #     when 'Indians'          then return 'Cleveland Indians'
+ #     when 'Cleveland'        then return 'Cleveland Indians'
+ #     when 'Tigers'           then return 'Detroit Tigers'
+ #     when 'Detroit'          then return 'Detroit Tigers'
+ #     when 'Royals'           then return 'Kansas City Royals'
+ #     when 'Kansas City'      then return 'Kansas City Royals'
+ #     when 'Twins'            then return 'Minnesota Twins'
+ #     when 'Minnesota'        then return 'Minnesota Twins'
+ #     when 'Cubs'             then return 'Chicago Cubs'
+      #
+  #    when 'Reds'             then return 'Cincinnati Reds'
+  #    when 'Astros'           then return 'Houston Astros'
+  #    when 'Brewers'          then return 'Milwaukee Brewers'
+  #    when 'Milwaukee'        then return 'Milwaukee Brewers'
+  #    when 'Pirates'          then return 'Pittsburgh Pirates'
+  #    when 'Pittsburgh'       then return 'Pittsburgh Pirates'
+  #    when 'Cardinals'        then return 'St. Louis Cardinals'
+  #    when 'St. Louis'        then return 'St. Louis Cardinals'
+  #    when 'Angels'           then return 'Los Angeles Angels'
+      #
+  #    when 'Athletics'        then return 'Oakland Athletics'
+  #    when 'Oakland'          then return 'Oakland Athletics'
+  #    when 'Mariners'         then return 'Seattle Mariners'
+  #    when 'Seattle'          then return 'Seattle Mariners'
+  #    when 'Rangers'          then return 'Texas Rangers'
+  #    when 'Texas'            then return 'Texas Rangers'
+  #    when 'Diamondbacks'     then return 'Arizona Diamondbacks'
+  #    when 'Arizona'          then return 'Arizona Diamondbacks'
+  #    when 'Rockies'          then return 'Colorado Rockies'
+  #    when 'Colorado'         then return 'Colorado Rockies'
+  #    when 'Dodgers'          then return 'Los Angeles Dodgers'
+      #
+  #    when 'Padres'           then return 'San Diego Padres'
+  #    when 'San Diego'        then return 'San Diego Padres'
+  #    when 'Giants'           then return 'San Francisco Giants'
+  #    when 'San Francisco'    then return 'San Francisco Giants'
+  #    end
+  #  end
+    
+  #  def self.full_team_name_NBA( team )
+  #    case team
+  #      when "Atlanta"       then return "Atlanta Hawks"
+  #      when "Hawks"         then return "Atlanta Hawks"
+  #      when "Boston"        then return "Boston Celtics"
+  #      when "Celtics"       then return "Boston Celtics"
+  #      when "Charlotte"     then return "Charlotte Bobcats"
+  #      when "Bobcats"       then return "Charlotte Bobcats"
+  #      when "Chicago"       then return "Chicago Bulls"
+  #      when "Bulls"         then return "Chicago Bulls"
+  #      when "Cleveland"     then return "Cleveland Cavaliers"
+  #      when "Cavaliers"     then return "Cleveland Cavaliers"
+  #      when "Dallas"        then return "Dallas Mavericks"
+  #      when "Mavericks"     then return "Dallas Mavericks"
+  #      when "Denver"        then return "Denver Nuggets"
+  #      when "Nuggets"       then return "Denver Nuggets" 
+  #      when "Detroit"       then return "Detroit Pistons"
+  #      when "Pistons"       then return "Detroit Pistons"
+  #      when "Golden State"  then return "Golden State Warriors"
+  #      when "Warriors"      then return "Golden State Warriors"
+  #      when "Houston"       then return "Houston Rockets"
+  #      when "Rockets"       then return "Houston Rockets"
+  #      when "Indiana"       then return "Indiana Pacers"
+  #      when "Pacers"        then return "Indiana Pacers"
+  #      when "LA Clippers"   then return "Los Angeles Clippers"
+  #      when "Clippers"      then return "Los Angeles Clippers"
+  #      when "LA Lakers"     then return "Los Angeles Lakers"
+  #      when "Lakers"        then return "Los Angeles Lakers"
+  #      when "Memphis"       then return "Memphis Grizzlies"
+  #      when "Grizzlies"     then return "Memphis Grizzlies"
+  #      when "Miami"         then return "Miami Heat"
+  #      when "Heat"          then return "Miami Heat"
+  #      when "Milwaukee"     then return "Milwaukee Bucks"
+  #      when "Bucks"         then return "Milwaukee Bucks"
+  #      when "Minnesota"     then return "Minnesota Timberwolves"
+  #      when "Timberwolves"  then return "Minnesota Timberwolves"
+  #      when "New Jersey"    then return "New Jersey Nets"
+  #      when "Nets"          then return "New Jersey Nets"
+  #      when "New Orleans"   then return "New Orleans Hornets"
+  #      when "Hornets"       then return "New Orleans Hornets"
+  #      when "New York"      then return "New York Knicks"
+  #      when "NY Knicks"     then return "New York Knicks"
+  #      when "Knicks"        then return "New York Knicks"
+  #      when "Oklahoma City" then return "Oklahoma City Thunder"
+  #      when "Thunder"       then return "Oklahoma City Thunder"
+  #      when "Orlando"       then return "Orlando Magic"
+  #      when "Magic"         then return "Orlando Magic"
+  #      when "Philadelphia"  then return "Philadelphia 76ers"
+  #      when "76ers"         then return "Philadelphia 76ers"
+  #      when "Phoenix"       then return "Phoenix Suns"
+  #      when "Suns"          then return "Phoenix Suns"
+  #      when "Portland"      then return "Portland Trail Blazers"
+  #      when "Trail Blazers" then return "Portland Trail Blazers"
+  #      when "Sacramento"    then return "Sacramento Kings"
+  #      when "Kings"         then return "Sacramento Kings"
+  #      when "San Antonio"   then return "San Antonio Spurs"
+  #      when "Spurs"         then return "San Antonio Spurs"
+  #      when "Toronto"       then return "Toronto Raptors"
+  #      when "Raptors"       then return "Toronto Raptors"
+  #      when "Utah"          then return "Utah Jazz"
+  #      when "Jazz"          then return "Utah Jazz"
+  #      when "Washington"    then return "Washington Wizards"
+ #       when "Wizards"       then return "Washington Wizards"
+ #     end
+ #   end
 
 end
