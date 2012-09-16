665acb67e33f91633ce393bf945710ac30c87c51
Elliot P. Harik <elliot.harik@gmail.com>
2012-09-16 13:03:48 -0500

    nil check on game scores

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -8,12 +8,8 @@ class Bet < ActiveRecord::Base
 
   def winner?
     game = Game.find(game_id)
-    if game.home_score.nil?
-      game.home_score = 0 
-    end
-    if game.away_score.nil?
-      game.away_score = 0
-    end
+    game.home_score = 0 if game.home_score.nil?
+    game.away_score = 0 if game.away_score.nil?
     total_score = game.home_score + game.away_score
 
     if bet_type == "under"
